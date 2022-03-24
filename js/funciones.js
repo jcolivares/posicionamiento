6149c292383c8dbac6753fb562b47f3a4e5ea8b9
PhD Juan Carlos Olivares Rojas <jcolivares@itmorelia.edu.mx>
2022-03-24 09:09:08 -0600

    Agregadno Javascript

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+//Ejemplo de javascript
+
+
+
+function main(){
+    //alert("Hola mundo!!!");
+    
+    var titulo = document.getElementById("titulo")
+    titulo.addEventListener(presioname(), "click", false)
+    
+    
+}
+
+window.addEventListener(main(), "load", false)
+
+function presioname(){
+    alert("entrando...")
+    var sem = document.getElementById("semestre");
+}
