5e2ef9d53c905125c7a2bdc4a66c5e34834bd6fa
PhD Juan Carlos Olivares Rojas <jcolivares@itmorelia.edu.mx>
2022-04-02 11:27:32 -0600

    Manejo de JS y Media Queries

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,73 @@
 //Ejemplo de javascript
 
+function generarAleatorio(min, max){
+    let numero = Math.random()*max
+    if(numero<min)
+        numero+=min
+    
+    return Math.floor(numero);
+}
 
+function generarToken(){
+    
+    let limite = 12
+    let token=""
+    
+    for(i=0; i<limite; i++){
+        let numero = generarAleatorio(32,128)
+        caracter = String.fromCharCode(numero)
+        token+=caracter
+    }
+    
+    return token;
+}
+
+function escribir(){
+    nombre = document.getElementById("nombre")
+    n = nombre.value
+    //alert("Escribiendo: "+n)
+    nombre.value=nombre.value.toUpperCase()
+        
+    sem = document.getElementById("sem")
+    sem.innerHTML="<p>"+n+"</p>"
+}
 
 function main(){
     //alert("Hola mundo!!!");
     
     var titulo = document.getElementById("titulo")
-    titulo.addEventListener(presioname(), "click", false)
+    titulo.addEventListener("click", presioname)
+    
+    titulo.innerHTML+="<p>"+generarToken()+"</p>"
+    
+    var semestre = document.getElementById("semestre")
+    semestre.addEventListener("change", function(){
+        //let sem = semestre.value
+        let s = this.value
+        //alert('Cambiando...'+s)
+        
+        let sem = document.getElementById("sem")
+        sem.innerHTML="<h1>"+s+"</h1>"
+        
+        s = parseInt(s)
+        
+        /*
+        if(s%2==0){
+            sem.style="background: red";
+        }else{
+            sem.style="background: yellow";
+        }
+        */
+        
+        if(s%2==0){
+            sem.className="par";
+        }else{
+            sem.className="impar"
+        }
+    })
+    
+    let nombre = document.getElementById("nombre")
+    nombre.addEventListener("keyup", escribir)
     
     
 }
